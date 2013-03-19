e12261f3a62cb9fc53fbbbf04964b3d78d4027a1
Fatih YILMAZ <fthylmz@live.com>
2013-03-20 00:37:37 +0200

    Fixed: actual_price returns Spree::Money class, used to_s to get "actual price"

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -5,7 +5,7 @@ Spree::Variant.class_eval do
   attr_accessible :option_values
 
   def to_hash(current_currency)
-    actual_price  = self.price_in(current_currency).display_price
+    actual_price  = self.price_in(current_currency).display_price.to_s
     #actual_price += Calculator::Vat.calculate_tax_on(self) if Spree::Config[:show_price_inc_vat]
     {
       :id    => self.id,
